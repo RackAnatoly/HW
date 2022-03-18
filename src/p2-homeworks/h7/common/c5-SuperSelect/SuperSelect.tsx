221d99d7ba6d4dc343c92c9f8b6f78c8b8c8851a
Anatoli Rak <Rack.anatoly@gmail.com>
2022-03-18 21:56:50 +0100

    hm6

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -9,21 +9,32 @@ type SuperSelectPropsType = DefaultSelectPropsType & {
 
 const SuperSelect: React.FC<SuperSelectPropsType> = (
     {
-        options,
-        onChange, onChangeOption,
+        options,onChange,
+        value, onChangeOption,
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions = options?.map((t,i)=>{
+        return (<option key={i} value={t}>{t}</option> )
+    }); // map options with key
+
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
     return (
-        <select onChange={onChangeCallback} {...restProps}>
+        <select value={value} onChange={onChangeCallback} {...restProps}>
             {mappedOptions}
         </select>
+
+    // <select value={color} onChange={handleChange}>
+    //     {colors.map(item => {
+    //         return (<option key={item.value} value={item.value}>{item.text}</option>);
+    //     })}
+    // </select>
     )
 }
 
